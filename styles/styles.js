4868bbef6f576c595cfc4423986e5e546aab189b
Juan David Franco <jfrancoh5@gmail.com>
2024-05-02 16:41:19 -0500

    some aadvances on theme customization

--- styles/styles.js
+++ styles/styles.js
@@ -31,15 +31,12 @@ const toggleHeaderGear = () =>{
     const nabvarListSettings = {
         generalSettings: document.querySelector('#general-settings'),
         navbarGear: document.querySelector('#header-settings-gear')
-
      }
 
      const { generalSettings,navbarGear } = nabvarListSettings;
      navbarGear.classList.toggle('header-settings-gear-change')
      generalSettings.classList.toggle('general-settings-change')
 }
-
-
 
 const addClassInformation = (projectTitleInfo,projectTitleD,projectTitleInformationChanger,projectTitleDelayChanger) =>{
     const projectAppTemp = {
@@ -66,3 +63,59 @@ const removeClassInformation = (projectTitleInfo,projectTitleDelayer,projectTitl
    projectTitleDelay.classList.replace(projectTitleDelayChanger,projectTitleDelayer) 
 }
 
+/*funciones de cambio de los estilos*/ 
+const addPruprleTheme = () =>{
+    /*identificar al selector de los estilos*/ 
+    let theme = document.querySelector(':root');
+    let header = document.querySelector('.header');
+    let spanTitleLogo = document.querySelector('.title-logo span')
+    let spanTitle = document.querySelector('.title-logo')
+    let navbarCustomizationTitle = document.querySelector('.navbar-customization h4')
+
+    /*cambiar el selector de estilos*/
+    theme.style.setProperty('--main-body-background','linear-gradient(90deg, rgb(57, 12, 81) 0%, rgb(81.86, 38.13, 105.19) 50%, rgb(57, 12, 81) 100%)')
+    theme.style.setProperty('--font-principal-letter','#FEFEFE')
+    theme.style.setProperty('--font-principal-title','#00D8FF')
+    theme.style.setProperty('--dark-principal-background','#2A093C')
+    header.style.setProperty('background-color','#2A093C')
+    spanTitle.style.setProperty('color','#FEFEFE')
+    spanTitleLogo.style.setProperty('color','#00D8FF')
+    navbarCustomizationTitle.style.setProperty('color','#00D8FF')
+}
+
+const addBlueTheme = () =>{
+    /*identificar al selector de los estilos*/ 
+    let theme = document.querySelector(':root');
+    let spanTitleLogo = document.querySelector('.title-logo span')
+    let spanTitle = document.querySelector('.title-logo')
+    let header = document.querySelector('.header')
+    let navbarCustomizationTitle = document.querySelector('.navbar-customization h4')
+
+    /*cambiar el selector de estilos*/
+    theme.style.setProperty('--main-body-background','linear-gradient(90deg, #0C0F51 0%, #22256D 50%, #0C0F51 100%)')
+    theme.style.setProperty('--font-principal-letter','#FEFEFE')
+    theme.style.setProperty('--font-principal-title','#8FFF00')
+    theme.style.setProperty('--dark-principal-background','#090B36')
+    header.style.setProperty('background-color','#090B36')
+    generalSettingChange.style.setProperty('background-color','#090B36')
+    navbarCustomizationTitle.setProperty('color','#8FFF00')
+    spanTitleLogo.style.setProperty('color','#8FFF00')
+}
+
+const addLightTheme = () =>{
+    let theme = document.querySelector(':root');
+    let spanTitleLogo = document.querySelector('.title-logo span')
+    let spanTitle = document.querySelector('.title-logo')
+    let header = document.querySelector('.header')
+    let generalSettingChange = document.querySelector('.general-settings-change')
+    let navbarCustomizationTitle = document.querySelectorAll('.general-settings h4')
+
+
+    theme.style.setProperty('--main-body-background','linear-gradient(90deg, #F7F7F7 0%, #E4E2E1 50%, #F7F7F7 100%)')
+    header.style.setProperty('background-color','#1E1E1E')
+    spanTitleLogo.style.setProperty('color','#00D8FF')
+    
+    spanTitle.style.setProperty('color','#FEFEFE')
+    generalSettingChange.style.setProperty('background-color','#1E1E1E')
+    navbarCustomizationTitle.style.setProperty('color','#00D8FF')
+}
