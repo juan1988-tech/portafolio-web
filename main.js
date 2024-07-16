2391cd3fc0a5ebef4ee603a68818a07aa04d2f27
Juan David Franco <jfrancoh5@gmail.com>
2024-07-16 17:38:20 -0500

    cambios de idioma hechos

--- main.js
+++ main.js
@@ -141,6 +141,101 @@ const introduction_changer = (request) =>{
     } 
 }
 
+const about_me_changer = (request) =>{
+    //seleccionar los valores de la API
+    const { about_me } = request;
+    const about_me_arr = Object.values(about_me);
+
+    //seleccionar todas las etiquetas del nodo introduction y cambiar
+    const about_me_node = document.getElementById('about-me');
+    const about_me_role = document.querySelector('.about-me-role');
+    const aobut_first_p = document.querySelector('.about-first-p');
+    const about_second_p = document.querySelector('.about-second-p');
+    
+    let introduction_arr = [about_me_node,about_me_role,aobut_first_p,about_second_p];
+    
+    try{
+        for(let i=0;i<=introduction_arr.length;i++){
+            introduction_arr[i].textContent =  about_me_arr[i];
+        }
+    }catch(err){
+        {}
+    } 
+}
+
+const project_changer = (request) =>{
+    const { projects } = request;
+
+    //obtener el contenido del título
+    const { projects_tittle } = projects;
+
+    //obtener el contenido de proyecto: app del clima
+    const { project_wather_app } = projects;
+    const { project_title_h4_weather_app, project_title_weather_description  } = project_wather_app;
+
+    //obtener el contenido de proyecto: libreria comunitaria
+    const { library_static_page } = projects;
+    const { project_title_h4_library, project_title_library_description } = library_static_page;
+
+    //obtener el contenido de proyecto: prueba tecnica Aranda
+    const { react_technical_proof } = projects;
+    const { project_title_h4_react_proof, project_title_react__proof_description } = react_technical_proof
+
+    let introduction_arr = [projects_tittle,project_title_h4_weather_app
+        ,project_title_weather_description,project_title_h4_library, project_title_library_description,
+        project_title_h4_react_proof, project_title_react__proof_description];
+ 
+    //seleccionar las etiquetas: titulo
+    
+    const project_title = document.querySelector('#projects')
+    //Aplicacion del clima React.js
+    const project_app_temp_title = document.querySelector('#project-title-delay h4');
+    const project_app_temp_p = document.querySelector('#project-title-delay p')
+    //Biblioteca
+    const project_lib_temp_title = document.querySelector('#project-library-information h4');
+    const project_lib_temp_p = document.querySelector('#project-library-information p');
+    //Prueba técnica de React
+    const project_react_proof_title = document.querySelector('#project-aranda-delay h4')
+    const project_react_proof_p = document.querySelector('#project-aranda-delay p')       
+    
+    let introduction_node = [project_title,project_app_temp_title,project_app_temp_p,project_lib_temp_title,project_lib_temp_p,project_react_proof_title,project_react_proof_p]
+
+    try {
+        for(i=0;i<=introduction_node.length;i++){
+            introduction_node[i].textContent = introduction_arr[i]
+        }
+    } catch (error) {
+        {}
+    }
+}
+
+const skills = (request) =>{
+    //seleccionar el contenido 
+    const { skills } = request;
+
+    //selecciona la etiqueta
+    const skill_node = document.querySelector('#skills h4');
+    skill_node.textContent = skills;
+}
+
+const contact = (request) =>{
+    //seleccionar el contenido
+    const { contact } = request
+    const contact_arr = Object.values(contact);
+    
+    const footer_tittle = document.querySelector('.footer h1');
+    const footer_p = document.querySelector('.footer h1+ p');
+
+    let footer_arr = [footer_tittle,footer_p]
+    
+    try {
+        for(i=0;i<=contact_arr.length;i++){
+            footer_arr[i].textContent = contact_arr[i]
+        }   
+    } catch (error) {
+        {}
+    }
+}
 
 const spanishCall = async () =>{
     await fetch('http://localhost:3005/esp')
@@ -151,6 +246,10 @@ const spanishCall = async () =>{
         setting_options_changer(respond);
         setting_options_changer_desk(respond);
         introduction_changer(respond);
+        about_me_changer(respond);
+        project_changer(respond);
+        skills(respond);
+        contact(respond);
     })
 }
    
@@ -163,6 +262,10 @@ const englishCall = async () =>{
         setting_options_changer(respond);
         setting_options_changer_desk(respond);
         introduction_changer(respond);
+        about_me_changer(respond);
+        project_changer(respond);
+        skills(respond);
+        contact(respond);
     })    
 }
 
