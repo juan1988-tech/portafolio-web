267f52692684bb05fa73bc5ed5432a50c5d1843b
Juan David Franco <jfrancoh5@gmail.com>
2025-08-24 17:22:37 -0500

    cambios provisionales media query desktop

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -18,6 +18,8 @@ const toggleButtonBurguer = () => {
     navbar.classList.toggle('navbar-change');
 };
 /*función de sroll para la sección de proyectos: versión cellphone*/
+//variable global que identifica al elemento body
+const bodyLayout = document.body;
 //1. identificar en el DOM al nodo de los proyectos con el evento de tocar pantalla en typescript
 const projectsContainerCard = document.getElementById('projects-container-card');
 const projectsContainerCardDelay = document.querySelector('#projects-container-card-delay');
@@ -44,78 +46,85 @@ projectsContainerCardDelay.classList.add(proyectContainerDelay[0]);
 //Hacer una referencia del primer toque, definiendo el toque en el eje x y el eje y
 projectsContainerCard.addEventListener('touchstart', (event) => {
     var _a;
-    let initClientX = (_a = event === null || event === void 0 ? void 0 : event.touches[0]) === null || _a === void 0 ? void 0 : _a.clientX;
-    startTouch = initClientX;
+    if ((bodyLayout === null || bodyLayout === void 0 ? void 0 : bodyLayout.clientWidth) <= 743) {
+        console.log('esta funcion solo se aplica hasta media query cellphone');
+        let initClientX = (_a = event === null || event === void 0 ? void 0 : event.touches[0]) === null || _a === void 0 ? void 0 : _a.clientX;
+        startTouch = initClientX;
+    }
 });
 projectsContainerCard.addEventListener('touchmove', (event) => {
-    const secondTouches = event === null || event === void 0 ? void 0 : event.touches[0].clientX;
-    //definir la matriz
-    const matrixTouch = {
-        initTouchX: startTouch,
-        secondTouchX: secondTouches,
-    };
-    //asignar la matriz a la variable
-    touchArrayMatrix = matrixTouch;
+    if (bodyLayout.clientWidth <= 743) {
+        const secondTouches = event === null || event === void 0 ? void 0 : event.touches[0].clientX;
+        //definir la matriz
+        const matrixTouch = {
+            initTouchX: startTouch,
+            secondTouchX: secondTouches,
+        };
+        //asignar la matriz a la variable
+        touchArrayMatrix = matrixTouch;
+    }
 });
 projectsContainerCard.addEventListener('touchend', () => {
-    const { initTouchX, secondTouchX } = touchArrayMatrix;
-    if (initTouchX > secondTouchX) {
-        touchMovement = touchMovement + 1;
-    }
-    if (initTouchX < secondTouchX) {
-        touchMovement = touchMovement - 1;
-    }
-    if (touchMovement > 3) {
-        touchMovement = 3;
-    }
-    if (touchMovement < 0) {
-        touchMovement = 0;
-    }
-    switch (touchMovement) {
-        case 0:
-            //cambio de estilo: carrusel para proyecto: 1
-            projectsContainerCardDelay.classList.replace(proyectContainerDelay[1], proyectContainerDelay[0]);
-            projectsContainerCardDelay.classList.replace(proyectContainerDelay[2], proyectContainerDelay[0]);
-            projectsContainerCardDelay.classList.replace(proyectContainerDelay[3], proyectContainerDelay[0]);
-            //cambio de estilos para los botones del carrusel
-            projectCaruoselCircles[0].classList.replace('carousel-indicator', 'outlined');
-            projectCaruoselCircles[1].classList.replace('outlined', 'carousel-indicator');
-            projectCaruoselCircles[2].classList.replace('outlined', 'carousel-indicator');
-            projectCaruoselCircles[3].classList.replace('outlined', 'carousel-indicator');
-            break;
-        case 1:
-            //cambio de estilo: carrusel para proyecto: 2
-            projectsContainerCardDelay.classList.replace(proyectContainerDelay[0], proyectContainerDelay[1]);
-            projectsContainerCardDelay.classList.replace(proyectContainerDelay[2], proyectContainerDelay[1]);
-            projectsContainerCardDelay.classList.replace(proyectContainerDelay[3], proyectContainerDelay[1]);
-            //cambio de estilos para los botones del carrusel
-            projectCaruoselCircles[0].classList.replace('outlined', 'carousel-indicator');
-            projectCaruoselCircles[1].classList.replace('carousel-indicator', 'outlined');
-            projectCaruoselCircles[2].classList.replace('outlined', 'carousel-indicator');
-            projectCaruoselCircles[3].classList.replace('outlined', 'carousel-indicator');
-            break;
-        case 2:
-            //cambio de estilo: carrusel para proyecto: 3
-            projectsContainerCardDelay.classList.replace(proyectContainerDelay[0], proyectContainerDelay[2]);
-            projectsContainerCardDelay.classList.replace(proyectContainerDelay[1], proyectContainerDelay[2]);
-            projectsContainerCardDelay.classList.replace(proyectContainerDelay[3], proyectContainerDelay[2]);
-            //cambio de estilos para los botones del carrusel
-            projectCaruoselCircles[0].classList.replace('outlined', 'carousel-indicator');
-            projectCaruoselCircles[1].classList.replace('outlined', 'carousel-indicator');
-            projectCaruoselCircles[2].classList.replace('carousel-indicator', 'outlined');
-            projectCaruoselCircles[3].classList.replace('outlined', 'carousel-indicator');
-            break;
-        case 3:
-            //cambio de estilo: carrusel para proyecto: 4
-            projectsContainerCardDelay.classList.replace(proyectContainerDelay[0], proyectContainerDelay[3]);
-            projectsContainerCardDelay.classList.replace(proyectContainerDelay[1], proyectContainerDelay[3]);
-            projectsContainerCardDelay.classList.replace(proyectContainerDelay[2], proyectContainerDelay[3]);
-            //cambio de estilos para los botones del carrusel
-            projectCaruoselCircles[0].classList.replace('outlined', 'carousel-indicator');
-            projectCaruoselCircles[1].classList.replace('outlined', 'carousel-indicator');
-            projectCaruoselCircles[2].classList.replace('outlined', 'carousel-indicator');
-            projectCaruoselCircles[3].classList.replace('carousel-indicator', 'outlined');
-            break;
+    if (bodyLayout.clientWidth <= 743) {
+        const { initTouchX, secondTouchX } = touchArrayMatrix;
+        if (initTouchX > secondTouchX) {
+            touchMovement = touchMovement + 1;
+        }
+        if (initTouchX < secondTouchX) {
+            touchMovement = touchMovement - 1;
+        }
+        if (touchMovement > 3) {
+            touchMovement = 3;
+        }
+        if (touchMovement < 0) {
+            touchMovement = 0;
+        }
+        switch (touchMovement) {
+            case 0:
+                //cambio de estilo: carrusel para proyecto: 1
+                projectsContainerCardDelay.classList.replace(proyectContainerDelay[1], proyectContainerDelay[0]);
+                projectsContainerCardDelay.classList.replace(proyectContainerDelay[2], proyectContainerDelay[0]);
+                projectsContainerCardDelay.classList.replace(proyectContainerDelay[3], proyectContainerDelay[0]);
+                //cambio de estilos para los botones del carrusel
+                projectCaruoselCircles[0].classList.replace('carousel-indicator', 'outlined');
+                projectCaruoselCircles[1].classList.replace('outlined', 'carousel-indicator');
+                projectCaruoselCircles[2].classList.replace('outlined', 'carousel-indicator');
+                projectCaruoselCircles[3].classList.replace('outlined', 'carousel-indicator');
+                break;
+            case 1:
+                //cambio de estilo: carrusel para proyecto: 2
+                projectsContainerCardDelay.classList.replace(proyectContainerDelay[0], proyectContainerDelay[1]);
+                projectsContainerCardDelay.classList.replace(proyectContainerDelay[2], proyectContainerDelay[1]);
+                projectsContainerCardDelay.classList.replace(proyectContainerDelay[3], proyectContainerDelay[1]);
+                //cambio de estilos para los botones del carrusel
+                projectCaruoselCircles[0].classList.replace('outlined', 'carousel-indicator');
+                projectCaruoselCircles[1].classList.replace('carousel-indicator', 'outlined');
+                projectCaruoselCircles[2].classList.replace('outlined', 'carousel-indicator');
+                projectCaruoselCircles[3].classList.replace('outlined', 'carousel-indicator');
+                break;
+            case 2:
+                //cambio de estilo: carrusel para proyecto: 3
+                projectsContainerCardDelay.classList.replace(proyectContainerDelay[0], proyectContainerDelay[2]);
+                projectsContainerCardDelay.classList.replace(proyectContainerDelay[1], proyectContainerDelay[2]);
+                projectsContainerCardDelay.classList.replace(proyectContainerDelay[3], proyectContainerDelay[2]);
+                //cambio de estilos para los botones del carrusel
+                projectCaruoselCircles[0].classList.replace('outlined', 'carousel-indicator');
+                projectCaruoselCircles[1].classList.replace('outlined', 'carousel-indicator');
+                projectCaruoselCircles[2].classList.replace('carousel-indicator', 'outlined');
+                projectCaruoselCircles[3].classList.replace('outlined', 'carousel-indicator');
+                break;
+            case 3:
+                //cambio de estilo: carrusel para proyecto: 4
+                projectsContainerCardDelay.classList.replace(proyectContainerDelay[0], proyectContainerDelay[3]);
+                projectsContainerCardDelay.classList.replace(proyectContainerDelay[1], proyectContainerDelay[3]);
+                projectsContainerCardDelay.classList.replace(proyectContainerDelay[2], proyectContainerDelay[3]);
+                //cambio de estilos para los botones del carrusel
+                projectCaruoselCircles[0].classList.replace('outlined', 'carousel-indicator');
+                projectCaruoselCircles[1].classList.replace('outlined', 'carousel-indicator');
+                projectCaruoselCircles[2].classList.replace('outlined', 'carousel-indicator');
+                projectCaruoselCircles[3].classList.replace('carousel-indicator', 'outlined');
+                break;
+        }
     }
 });
 /*función de sroll para la sección de proyectos: versión cellphone*/
@@ -143,48 +152,54 @@ serviceTargetGrid.classList.add(serviceContainerDelay[0]);
 //Hacer una referencia del primer toque, definiendo el toque en el eje x y el eje y
 serviceTargetContainer.addEventListener('touchstart', (event) => {
     var _a;
-    let initClientX = (_a = event === null || event === void 0 ? void 0 : event.touches[0]) === null || _a === void 0 ? void 0 : _a.clientX;
-    startServiceTouch = initClientX;
+    if (bodyLayout.clientWidth <= 743) {
+        let initClientX = (_a = event === null || event === void 0 ? void 0 : event.touches[0]) === null || _a === void 0 ? void 0 : _a.clientX;
+        startServiceTouch = initClientX;
+    }
 });
 serviceTargetContainer.addEventListener('touchmove', (event) => {
-    const secondTouches = event === null || event === void 0 ? void 0 : event.touches[0].clientX;
-    //definir la matriz
-    const matrixTouch = {
-        initTouchX: startServiceTouch,
-        secondTouchX: secondTouches,
-    };
-    //asignar la matriz a la variable
-    touchServiceMatrix = matrixTouch;
+    if (bodyLayout.clientWidth <= 743) {
+        const secondTouches = event === null || event === void 0 ? void 0 : event.touches[0].clientX;
+        //definir la matriz
+        const matrixTouch = {
+            initTouchX: startServiceTouch,
+            secondTouchX: secondTouches,
+        };
+        //asignar la matriz a la variable
+        touchServiceMatrix = matrixTouch;
+    }
 });
 serviceTargetContainer.addEventListener('touchend', () => {
-    const { initTouchX, secondTouchX } = touchServiceMatrix;
-    if (initTouchX > secondTouchX) {
-        touchServiceMovement = touchServiceMovement + 1;
-    }
-    if (initTouchX < secondTouchX) {
-        touchServiceMovement = touchServiceMovement - 1;
-    }
-    if (touchServiceMovement > 1) {
-        touchServiceMovement = 1;
-    }
-    if (touchServiceMovement < 0) {
-        touchServiceMovement = 0;
-    }
-    switch (touchServiceMovement) {
-        case 0:
-            //cambio de estilos para la tarjeta de servicios
-            serviceTargetGrid.classList.replace(serviceContainerDelay[1], serviceContainerDelay[0]);
-            serviceCarouselCircles[0].classList.replace('service-indicator', 'service-outlined');
-            serviceCarouselCircles[1].classList.replace('service-outlined', 'service-indicator');
-            break;
-        case 1:
-            //cambio de estilos para la tarjeta de servicios
-            serviceTargetGrid.classList.replace(serviceContainerDelay[0], serviceContainerDelay[1]);
-            serviceCarouselCircles[0].classList.replace('service-outlined', 'service-indicator');
-            serviceCarouselCircles[1].classList.replace('service-indicator', 'service-outlined');
-            break;
-        default:
-            0;
-            break;
+    if (bodyLayout.clientWidth <= 743) {
+        const { initTouchX, secondTouchX } = touchServiceMatrix;
+        if (initTouchX > secondTouchX) {
+            touchServiceMovement = touchServiceMovement + 1;
+        }
+        if (initTouchX < secondTouchX) {
+            touchServiceMovement = touchServiceMovement - 1;
+        }
+        if (touchServiceMovement > 1) {
+            touchServiceMovement = 1;
+        }
+        if (touchServiceMovement < 0) {
+            touchServiceMovement = 0;
+        }
+        switch (touchServiceMovement) {
+            case 0:
+                //cambio de estilos para la tarjeta de servicios
+                serviceTargetGrid.classList.replace(serviceContainerDelay[1], serviceContainerDelay[0]);
+                serviceCarouselCircles[0].classList.replace('service-indicator', 'service-outlined');
+                serviceCarouselCircles[1].classList.replace('service-outlined', 'service-indicator');
+                break;
+            case 1:
+                //cambio de estilos para la tarjeta de servicios
+                serviceTargetGrid.classList.replace(serviceContainerDelay[0], serviceContainerDelay[1]);
+                serviceCarouselCircles[0].classList.replace('service-outlined', 'service-indicator');
+                serviceCarouselCircles[1].classList.replace('service-indicator', 'service-outlined');
+                break;
+            default:
+                0;
+                break;
+        }
     }
 });
