bd73c40b64f13890fbefa37cf30a740f81317198
Juan David Franco <jfrancoh5@gmail.com>
2025-09-10 10:03:44 -0500

    seccion stack terminada

--- src/addThemes.ts
+++ src/addThemes.ts
@@ -35,6 +35,20 @@ interface theme{
             projectCarouselRightimg: HTMLImageElement
         }
     }
+    stack: {
+        stackBackground: HTMLDivElement,
+        stackContainerGrid: [
+            { image: HTMLImageElement },
+            { image: HTMLImageElement },
+            { image: HTMLImageElement },
+            { image: HTMLImageElement },
+            { image: HTMLImageElement },
+            { image: HTMLImageElement },
+            { image: HTMLImageElement },
+            { image: HTMLImageElement },
+            { image: HTMLImageElement },
+        ]
+    }
 }
 
 let itemsTheme: theme ={
@@ -81,6 +95,38 @@ let itemsTheme: theme ={
             projectCarouselLeftImg: document.getElementById('project-carousel-left-img') as HTMLImageElement,
             projectCarouselRightimg: document.getElementById('project-carousel-right-img') as HTMLImageElement
         } 
+    },
+    stack:{
+        stackBackground: document.querySelector("#stack") as HTMLDivElement,
+        stackContainerGrid: [
+            {
+                image: document.getElementById('mongo-db') as HTMLImageElement
+            },
+            {
+                image: document.getElementById('express') as HTMLImageElement
+            },
+            {
+                image: document.getElementById('react') as HTMLImageElement
+            },
+            {
+                image: document.getElementById('node') as HTMLImageElement
+            },
+            {
+                image: document.getElementById('typescript') as HTMLImageElement
+            },
+            {
+                image: document.getElementById('photoshop') as HTMLImageElement
+            },
+            {
+                image: document.getElementById('illustrator') as HTMLImageElement
+            },
+            {
+                image: document.getElementById('figma') as HTMLImageElement
+            },
+            {
+                image: document.getElementById('trello') as HTMLImageElement
+            },
+        ]
     }
 }
 
@@ -94,7 +140,7 @@ export const addBodyTheme = (mainBg:string,secondBg:string,thirdBg:string):void 
 }
 
 //funcion para modificar los colores del header
-export const { header,themesLi,themesLiHeader,contactIcon,navbar,generalSettings,introduction,aboutMe,projects } = itemsTheme; 
+export const { header,themesLi,themesLiHeader,contactIcon,navbar,generalSettings,introduction,aboutMe,projects,stack } = itemsTheme; 
 
 const addheaderTheme = (mainColor:string,secondColor:string,thirdColor:string,contactIconImg:string):void =>{
     //header.classList.replace('header-blue','header');
@@ -326,11 +372,72 @@ const addProjectTheme = (bgColor:string) =>{
             projectCarouselRight.classList.replace("project-carousel-button","project-carousel-button-white");
             projectCarouselRight.classList.replace("project-carousel-button-blue","project-carousel-button-white");
 
-            projectCarouselLeftImg.src = '../public/assets/icons/new-icons/light-screen-icons/ep_arrow-left-bold-black.svg';
-            projectCarouselRightimg.src = '../public/assets/icons/new-icons/light-screen-icons/ep_arrow-rigtht-bold-black.svg';    
+            projectCarouselLeftImg.src = './assets/icons/new-icons/light-screen-icons/ep_arrow-left-bold-black.svg';
+            projectCarouselRightimg.src = './assets/icons/new-icons/light-screen-icons/ep_arrow-rigtht-bold-black.svg';    
             break;
     }
 }
+
+
+const addStackTheme = (bgColor:string):void =>{
+    const lightIconsStackUrls: string[] = [
+        "./assets/icons/new-icons/dark-screen-icons/devicon-plain_mongodb-wordmark.svg",
+        "./assets/icons/new-icons/dark-screen-icons/simple-icons_express.svg",
+        "./assets/icons/new-icons/dark-screen-icons/uil_react.svg",
+        "./assets/icons/new-icons/dark-screen-icons/fa-brands_node.svg",
+        "./assets/icons/new-icons/dark-screen-icons/catppuccin_typescript.svg",
+        "./assets/icons/new-icons/dark-screen-icons/streamline-logos_adobe-photoshop-logo-solid.svg",
+        "./assets/icons/new-icons/dark-screen-icons/streamline-logos_adobe-illustrator-logo-solid.svg",
+        "./assets/icons/new-icons/dark-screen-icons/solar_figma-broken.svg",
+        "./assets/icons/new-icons/dark-screen-icons/devicon-plain_trello-wordmark.svg"
+    ]
+
+    const darkIconStackUrls: string[] =[
+        "./assets/icons/new-icons/light-screen-icons/devicon-plain_mongodb-wordmark.svg",
+        "./assets/icons/new-icons/light-screen-icons/simple-icons_express.svg",
+        "./assets/icons/new-icons/light-screen-icons/uil_react.svg",
+        "./assets/icons/new-icons/light-screen-icons/fa-brands_node.svg",
+        "./assets/icons/new-icons/light-screen-icons/catppuccin_typescript.svg",
+        "./assets/icons/new-icons/light-screen-icons/streamline-logos_adobe-photoshop-logo-solid.svg",
+        "./assets/icons/new-icons/light-screen-icons/streamline-logos_adobe-illustrator-logo-solid.svg",
+        "./assets/icons/new-icons/light-screen-icons/solar_figma-broken.svg",
+        "./assets/icons/new-icons/light-screen-icons/devicon-plain_trello-wordmark.svg"
+    ]
+
+    const { stackBackground,stackContainerGrid } = stack;
+
+    const addSetImages = (stackContainerList:string[]) =>{
+        for(let i = 0; i<stackContainerGrid.length;i++){
+        const imageNode = stackContainerGrid[i].image;
+
+        const imageAttribute = stackContainerList[i];
+
+        imageNode.setAttribute("src",imageAttribute);
+
+        console.log(imageNode.getAttribute("src"))
+        }
+    }
+    
+
+    switch (bgColor) {
+        case "purple":
+            stackBackground.classList.replace("stack-blue","stack");
+            stackBackground.classList.replace("stack-white","stack");
+            addSetImages(lightIconsStackUrls);
+            break;
+        case "blue":
+            stackBackground.classList.replace("stack","stack-blue");
+            stackBackground.classList.replace("stack-white","stack-blue");
+            addSetImages(lightIconsStackUrls);
+            break;
+        case "white":
+            stackBackground.classList.replace("stack","stack-white");
+            stackBackground.classList.replace("stack-blue","stack-white");
+            addSetImages(darkIconStackUrls);
+            break;
+    }
+}
+
 
 export function addThemeLayout(bgColor:string){
     switch (bgColor) {
@@ -342,6 +449,7 @@ export function addThemeLayout(bgColor:string){
             addIntroductionTheme(bgColor);
             addboutMeTheme(bgColor);
             addProjectTheme(bgColor);
+            addStackTheme(bgColor);
             break;
         case "blue":
             addBodyTheme('body-blue','body','body-white');
@@ -351,6 +459,7 @@ export function addThemeLayout(bgColor:string){
             addIntroductionTheme(bgColor);
             addboutMeTheme(bgColor);
             addProjectTheme(bgColor);
+            addStackTheme(bgColor);
             break;
         case "white":
             addBodyTheme('body-white','body-blue','body');
@@ -360,6 +469,7 @@ export function addThemeLayout(bgColor:string){
             addIntroductionTheme(bgColor);
             addboutMeTheme(bgColor);
             addProjectTheme(bgColor);
+            addStackTheme(bgColor);
             break;    
     }
 
